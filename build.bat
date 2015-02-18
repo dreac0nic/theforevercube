1274ac66cde8c639a3111217067c0e3bc338bf43
drac0nic <spenser.m.bray@gmail.com>
2015-02-17 18:36:26 -0600

    Remove GL library from build path. It is unnecessary.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ ./src/main.cpp ./src/gl_comp_3_1.cpp -L./lib -I./include -lglfw3 -lopengl32 -lGL -lgdi32 -o ./build/ForeverCube.exe
+g++ ./src/main.cpp ./src/gl_comp_3_1.cpp -L./lib -I./include -lglfw3 -lopengl32  -lgdi32 -o ./ForeverCube.exe
