b3d3f8b14929c3efa19ec0bd452da5cf5b327856
drac0nic <spenser.m.bray@gmail.com>
2015-02-26 19:03:42 -0600

    Update batch to static stds, c++11, and core 3.3

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ ./src/main.cpp ./src/gl_comp_3_1.cpp -L./lib -I./include -lglfw3 -lopengl32  -lgdi32 -o ./ForeverCube.exe
+g++ ./src/main.cpp ./src/gl_core_3_3.cpp -static-libgcc -static-libstdc++ -L./lib -I./include -lglfw3 -lopengl32  -lgdi32 -o ./ForeverCube.exe -std=c++11
