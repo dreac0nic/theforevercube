17145b202d7d7617e2bdd0b724f933b375e2a30b
drac0nic <spenser.m.bray@gmail.com>
2015-03-01 00:00:24 -0600

    Add simple start to sierpinski gasket code.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "gl_comp_4_4.hpp"
 #include <GLFW/glfw3.h>
 
+#include "glslu.hpp"
+
 #define ERRLOG(errstr) std::cerr << "ERR [" << __FILE__ << ":" << __LINE__ << "] " << errstr << std::endl;
 
 void glfw_err_callback(int code, const char* message);
@@ -84,6 +86,50 @@ int main(int argc, char* argv[])
     
     cerr << "SYSTEM ... OK" << endl
 	 << "RUNNING" << endl;
+    
+    // Setup for Sierpinski Gasket
+    unsigned int pointCount = 1;
+    vec2* points;
+    vec2 vertices[3] = {
+	vec2(-1.0f, -1.0f),
+	vec2( 0.0f,  1.0f),
+	vec2( 1.0f, -1.0f)
+    };
+    
+    srand(time(NULL));
+    
+    // Get the number of points to generate
+    cout << "Please enter the number of points to generate: ";
+    cin >> pointCount;
+    
+    points = new vec2[pointCount];
+    
+    // Set first point and calculate the rest of the points from there.
+    points[0] = vec2(0.25f, 0.5f);
+    
+    for(int point = 1; point < pointCount; ++point) {
+	int vertex = rand()%3;
+	
+	points[point] = (points[point - 1] + vertices[vertex])/2.0f;
+    }
+    
+    GLuint vao;
+    gl::GenBuffers(1, &vao);
+    gl::BindVertexArray(vao);
+    
+    GLuint buffer;
+    gl::GenBuffers(1, &buffer);
+    gl::BindBuffer(gl::ARRAY_BUFFER, buffer);
+    gl::BufferData(gl::ARRAY_BUFFER, sizeof(points), points, gl::STATIC_DRAW);
+
+    Program basicProgram;
+    basicProgram.compileShader("passthrough.glsl.vert");
+    basicProgram.compileShader("red.glsl.frag");
+    
+    basicProgram.link();
+    
+    if(basicProgram.isLinked())
+	basicProgram.use();
 
     // Enter main loop of application.
     while(!glfwWindowShouldClose(hWindow)) {
