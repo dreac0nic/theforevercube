a51e419b3c186dd6fc9639be53ef6d4abde5bb25
drac0nic <spenser.m.bray@gmail.com>
2015-02-28 22:44:31 -0600

    Update the build script to 4.4

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ ./src/main.cpp ./src/gl_comp_3_1.cpp -L./lib -I./include -lglfw3 -lopengl32  -lgdi32 -o ./ForeverCube.exe
+g++ ./src/main.cpp ./src/gl_comp_4_4.cpp -L./lib -I./include -lglfw3 -lopengl32  -lgdi32 -o ./ForeverCube.exe
